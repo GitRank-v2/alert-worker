5f71a936c59d2e0fac00f13976a09ede4319d9d4
김승진 <ohksj77@gmail.com>
2024-12-20 19:26:00 +0900

    chore: init project

---
+++ src/test/kotlin/com/dragonguard/alert/AlertApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.dragonguard.alert
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlertApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
